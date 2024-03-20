6f24c57c671506226d151be09bc45f567e7e00a6
volant97 <volant97@naver.com>
2024-03-21 01:58:24 +0900

    [ADD] shadcn/ui 설치 및 기본 구성 파악
    Button과 Card를 다뤄보면서 어떻게 활용할 수 있는지 알게 되었다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,33 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main>안녕하세요</main>;
+  return (
+    <main className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Card>
+        <CardHeader>
+          <CardTitle>카드 타이틀</CardTitle>
+          <CardDescription>카드 설명</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div>폼</div>
+        </CardContent>
+        <CardFooter>
+          <Button>버튼1</Button>
+          <Button>버튼2</Button>
+        </CardFooter>
+      </Card>
+    </main>
+  );
 }
